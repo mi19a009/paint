3366d397b86b2fb8ce6198de99664adde57b60c1
Taichi Murakami <mi19a009@oecu.jp>
2025-03-25 00:24:31 +0900

    Visual Studio プロジェクトを作成

---
+++ win/Resource.h
@@ -0,0 +1,25 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ で生成されたインクルード ファイル。
+// Paint.rc で使用
+//
+#define IDR_MAIN                        2
+#define IDI_MAIN                        101
+#define IDD_DIALOG1                     105
+#define IDD_ABOUT                       301
+#define IDS_MAIN                        401
+#define IDS_DOCUMENT                    402
+#define IDM_HELP                        40001
+#define IDM_ABOUT                       40002
+#define IDM_CLOSE                       40003
+#define IDM_NEW                         40004
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        107
+#define _APS_NEXT_COMMAND_VALUE         40011
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
---
+++ win/Source/About.c
@@ -0,0 +1,60 @@
+﻿/*
+Copyright 2025 Taichi Murakami.
+バージョン情報ダイアログ プロシージャを実装します。
+*/
+
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#include "Source.h"
+#include "Resource.h"
+
+static INT_PTR CALLBACK DialogProc(_In_ HWND hDlg, _In_ UINT uMsg, _In_ WPARAM wParam, _In_ LPARAM lParam);
+static INT_PTR WINAPI OnClose(_In_ HWND hDlg, _In_ UINT uMsg, _In_ WPARAM wParam, _In_ LPARAM lParam);
+static INT_PTR WINAPI OnCommand(_In_ HWND hDlg, _In_ UINT uMsg, _In_ WPARAM wParam, _In_ LPARAM lParam);
+static INT_PTR WINAPI OnInitDialog(_In_ HWND hDlg, _In_ UINT uMsg, _In_ WPARAM wParam, _In_ LPARAM lParam);
+
+EXTERN_C BOOL WINAPI AboutDialog(_In_ LPABOUTDIALOG lpParam)
+{
+	return DialogBoxParam(lpParam->hInstance, MAKEINTRESOURCE(IDD_ABOUT), lpParam->hwndOwner, DialogProc, (LPARAM)lpParam) == IDOK;
+}
+
+static INT_PTR CALLBACK DialogProc(_In_ HWND hDlg, _In_ UINT uMsg, _In_ WPARAM wParam, _In_ LPARAM lParam)
+{
+	INT_PTR bResult;
+
+	switch (uMsg)
+	{
+	case WM_CLOSE:
+		bResult = OnClose(hDlg, uMsg, wParam, lParam);
+		break;
+	case WM_COMMAND:
+		bResult = OnCommand(hDlg, uMsg, wParam, lParam);
+		break;
+	case WM_INITDIALOG:
+		bResult = OnInitDialog(hDlg, uMsg, wParam, lParam);
+		break;
+	default:
+		bResult = FALSE;
+		break;
+	}
+
+	return bResult;
+}
+
+static INT_PTR WINAPI OnClose(_In_ HWND hDlg, _In_ UINT uMsg, _In_ WPARAM wParam, _In_ LPARAM lParam)
+{
+	EndDialog(hDlg, 0);
+	return FALSE;
+}
+
+static INT_PTR WINAPI OnCommand(_In_ HWND hDlg, _In_ UINT uMsg, _In_ WPARAM wParam, _In_ LPARAM lParam)
+{
+	EndDialog(hDlg, LOWORD(wParam));
+	return FALSE;
+}
+
+static INT_PTR WINAPI OnInitDialog(_In_ HWND hDlg, _In_ UINT uMsg, _In_ WPARAM wParam, _In_ LPARAM lParam)
+{
+	MoveWindowToCenter(hDlg, FALSE);
+	return TRUE;
+}
