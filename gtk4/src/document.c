6ac54d400185f95c41cd5643eebf6cced4d602bb
Taichi Murakami <mi19a009@oecu.jp>
2025-08-24 18:33:43 +0900

    UI テンプレートを作成した

--- gtk4/src/document.c
+++ gtk4/src/document.c
@@ -8,8 +8,11 @@ struct _PaintDocumentWindow
 	GtkApplicationWindow parent_instance;
 };
 
-static void paint_document_window_class_init (PaintDocumentWindowClass *window);
-static void paint_document_window_init       (PaintDocumentWindow *window);
+static void paint_document_window_class_init        (PaintDocumentWindowClass *window);
+static void paint_document_window_class_init_object (GObjectClass *object);
+static void paint_document_window_class_init_widget (GtkWidgetClass *widget);
+static void paint_document_window_dispose           (GObject *object);
+static void paint_document_window_init              (PaintDocumentWindow *window);
 
 /*******************************************************************************
 Paint Document Window クラス:
@@ -18,6 +21,7 @@ Paint Document Window クラス:
 */ G_DEFINE_FINAL_TYPE (PaintDocumentWindow, paint_document_window, GTK_TYPE_APPLICATION_WINDOW);
 #define PAINT_DOCUMENT_WINDOW_PROPERTY_APPLICATION_NAME "application"
 #define PAINT_DOCUMENT_WINDOW_PROPERTY_SHOW_MENUBAR_NAME "show-menubar"
+#define PAINT_DOCUMENT_WINDOW_TEMPLATE_NAME "/com/github/mi19a009/paint/gtk/document.ui"
 
 /*******************************************************************************
 Class Init:
@@ -25,6 +29,36 @@ Class Init:
 */ static void
 paint_document_window_class_init (PaintDocumentWindowClass *window)
 {
+	paint_document_window_class_init_object (G_OBJECT_CLASS (window));
+	paint_document_window_class_init_widget (GTK_WIDGET_CLASS (window));
+}
+
+/*******************************************************************************
+Class Init Object:
+Objectクラスを初期化します。
+*/ static void
+paint_document_window_class_init_object (GObjectClass *object)
+{
+	object->dispose = paint_document_window_dispose;
+}
+
+/*******************************************************************************
+Class Init Widget:
+Widget クラスを初期化します。
+*/ static void
+paint_document_window_class_init_widget (GtkWidgetClass *widget)
+{
+	gtk_widget_class_set_template_from_resource (widget, PAINT_DOCUMENT_WINDOW_TEMPLATE_NAME);
+}
+
+/*******************************************************************************
+Init:
+クラスのインスタンスを破棄します。
+*/ static void
+paint_document_window_dispose (GObject *object)
+{
+	gtk_widget_dispose_template (GTK_WIDGET (object), PAINT_TYPE_DOCUMENT_WINDOW);
+	G_OBJECT_CLASS (paint_document_window_parent_class)->dispose (object);
 }
 
 /*******************************************************************************
@@ -33,6 +67,7 @@ Init:
 */ static void
 paint_document_window_init (PaintDocumentWindow *window)
 {
+	gtk_widget_init_template (GTK_WIDGET (window));
 }
 
 /*******************************************************************************
